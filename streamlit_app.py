d5931ab13562c986e6f2d1074533158e93ec1181
arpit-code1993 <54872550+arpit-code1993@users.noreply.github.com>
2024-03-18 14:26:48 -0400

    Update streamlit_app.py
    Changed the title and added breakfast menu as header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,15 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title("My Parents New Healthy Diner")
+st.header('Breakfast Menu')
+
+# Display the breakfast menu items
+st.write('Omega 3 & Blueberry Oatmeal')
+st.write('Kale, Spinach & Rocket Smoothie')
+st.write('Hard-Boiled Free-Range Egg')
+
+
 st.write(
     """Choose the fruits you want in your custome Smoothie!
     """
